42e9d5366637dc5cc8363b57bc5298df2fe827be
Emily Claps <eclaps@idmod.org>
2023-05-15 11:12:44 -0700

    pushing anonymized env_config.R config file

---
+++ env_config.R
@@ -0,0 +1,19 @@
+## Configuration file containing all user/context specific parameters.
+## Note: Paths must end in a forward-slash.
+#######################
+
+# Full path to project directory containing project_config.R and any project-specific functions.
+# Ex: "C:/Users/johnsmith/SAE/SmallAreaEstimationForSurveyIndicators/Project_Examples/FP_Age/"
+project_path <- "insert path here"
+
+# Full path to location of the Main.R on your platform
+# Ex: "C:/Users/johnsmith/SAE/SmallAreaEstimationForSurveyIndicators/"
+SAEDir <- "insert path here"
+
+# Full path to directory containing two folders: DHS and shapefiles. Put the required DHS data in the DHS folder and the GADM shapefile data into shapefiles.
+# Ex: "C:/Users/johnsmith/SAE/RawData/"
+RawDataDir <- "insert path here"
+
+# Full path to desired location of output of each project.
+# Ex: "C:/Users/johnsmith/SAE/ProjectResults/"
+projectResultsDir <- "insert path here"
